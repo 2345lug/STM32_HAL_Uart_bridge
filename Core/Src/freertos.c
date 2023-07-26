47a19b3d643682f14e373de9bc4ec873d79f1aba
Grigorovich Sergey <sergey.grigorovich@promwad.com>
2023-07-27 00:50:35 +0300

    feat: initial add configured cubemx project

---
+++ Core/Src/freertos.c
@@ -0,0 +1,175 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+
+/* USER CODE END Variables */
+/* Definitions for xUart1Transmit */
+osThreadId_t xUart1TransmitHandle;
+const osThreadAttr_t xUart1Transmit_attributes = {
+  .name = "xUart1Transmit",
+  .stack_size = 512 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for xUart2Transmit */
+osThreadId_t xUart2TransmitHandle;
+const osThreadAttr_t xUart2Transmit_attributes = {
+  .name = "xUart2Transmit",
+  .stack_size = 512 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for xUart1RecievedQueue */
+osMessageQueueId_t xUart1RecievedQueueHandle;
+const osMessageQueueAttr_t xUart1RecievedQueue_attributes = {
+  .name = "xUart1RecievedQueue"
+};
+/* Definitions for xUart2RecievedQueue */
+osMessageQueueId_t xUart2RecievedQueueHandle;
+const osMessageQueueAttr_t xUart2RecievedQueue_attributes = {
+  .name = "xUart2RecievedQueue"
+};
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+void xUart1TransmitTask(void *argument);
+void xUart2TransmitTask(void *argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* Create the queue(s) */
+  /* creation of xUart1RecievedQueue */
+  xUart1RecievedQueueHandle = osMessageQueueNew (16, sizeof(uint8_t), &xUart1RecievedQueue_attributes);
+
+  /* creation of xUart2RecievedQueue */
+  xUart2RecievedQueueHandle = osMessageQueueNew (16, sizeof(uint8_t), &xUart2RecievedQueue_attributes);
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of xUart1Transmit */
+  xUart1TransmitHandle = osThreadNew(xUart1TransmitTask, NULL, &xUart1Transmit_attributes);
+
+  /* creation of xUart2Transmit */
+  xUart2TransmitHandle = osThreadNew(xUart2TransmitTask, NULL, &xUart2Transmit_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+}
+
+/* USER CODE BEGIN Header_xUart1TransmitTask */
+/**
+  * @brief  Function implementing the xUart1Transmit thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_xUart1TransmitTask */
+void xUart1TransmitTask(void *argument)
+{
+  /* USER CODE BEGIN xUart1TransmitTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END xUart1TransmitTask */
+}
+
+/* USER CODE BEGIN Header_xUart2TransmitTask */
+/**
+* @brief Function implementing the xUart2Transmit thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_xUart2TransmitTask */
+void xUart2TransmitTask(void *argument)
+{
+  /* USER CODE BEGIN xUart2TransmitTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END xUart2TransmitTask */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+/* USER CODE END Application */
+
